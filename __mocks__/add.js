f06e95d513bbdcc8cbd020c9392edeb44aba3908
Abby Nyakara <abbynyakara@gmail.com>
2022-06-16 11:31:39 +0300

    correct linter errors

--- __mocks__/add.js
+++ __mocks__/add.js
@@ -1,10 +1,8 @@
-const addToDo = (list, item) => {
-  return (list.push(item));
-}
+const addToDo = (list, item) => list.push(item);
 
 const removeToDo = (listArray, index) => {
   listArray.splice(index, 1);
   return listArray;
 }
 
-export { addToDo, removeToDo } ;
+export { addToDo, removeToDo };
--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-alert */
+
 import 'lodash';
 import './style.css';
 import removeTodo from './ui.js';
